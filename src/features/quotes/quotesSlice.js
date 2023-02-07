589f6abd97fb1aa096903d3e7e78d73d54a1af61
Craig Ford <craig_ford@icloud.com>
2023-02-07 13:05:13 -0500

    commit tree ae1e86f6c1e21f273843edc143940b729a9a8ccb

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,9 +1,64 @@
 // Action Creators
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  return {
+    type: "quotes/add",
+    payload: quote
+  };
+};
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: "quotes/remove",
+    payload: quoteId
+  };
+};
+
+export const upvoteQuote = (quoteid) => {
+  return {
+    type: "quotes/upvote",
+    payload: quoteid
+  };
+};
+
+export const downvoteQuote = (quoteId) => {
+  return {
+    type: "quotes/downvote",
+    payload: quoteId
+  };
+};
 
 // Reducer
 const initialState = [];
 
 export default function quotesReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case "quotes/add":
+      return [...state, action.payload];
+
+    case "quotes/remove":
+      return state.filter((q) => q.id !== action.payload);
+
+    case "quotes/upvote":
+      return state.map((quote) => {
+          if (quote.id === action.payload) {
+            return {...quote, votes: quote.votes + 1};
+          } else {
+            return quote;
+          }
+      });
+
+      case "quotes/downvote":
+        return state.map((quote) => {
+            if (quote.id === action.payload  && quote.votes > 0) {
+              return {...quote, votes: quote.votes - 1};
+            } else {
+              return quote;
+            }
+        });
+
+      default:  
+        break;
+  }  
+  return state;  
 }
